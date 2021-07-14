e3febc55911a48e69f3f27f3d02ab32ddebc9cf5
Aitor Iturrioz <riturrioz@gmail.com>
2021-07-14 14:00:23 +0200

    Set barcode length to 9 characters
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/student.py
+++ src/student.py
@@ -6,18 +6,23 @@ logger = logging.getLogger(__name__)
 
 
 class Student(object):
+
+    BARCODE_LENGTH = 9 # 8 digits + 1 letter
+
     def __init__(self, student_id, user_id="", name="", identification_code="", student_code="", courses=[], rfid_code=''):
         self.student_id = student_id
         self.user_id = user_id
         self.name = name
         self.identification_code = identification_code
-        self.student_code = student_code
+        self.student_code = str(student_code)
         self.courses = courses
         self.rfid_code = rfid_code
 
     def get_barcode(self):
         if self.student_code:
-            return self.student_code
+            previous_zeros = self.BARCODE_LENGTH - len(self.student_code)
+            prefix = "0" * previous_zeros
+            return prefix + self.student_code
         else:
             return self.identification_code
 
